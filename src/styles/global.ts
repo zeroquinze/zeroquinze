fa003d48a456076f320af5b5deb8407be3242f56
Léo Campos <cmpsleo3@gmail.com>
2021-03-09 14:00:25 -0300

    feat: added body font-family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,4 +8,8 @@ export const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
 `;
